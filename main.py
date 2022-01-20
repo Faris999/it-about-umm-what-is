b73dc0f5c953453e8e6074581713f75b1c1317c2
Faris Hafizhan Hakim <farishafiz999@gmail.com>
2022-01-20 22:28:10 +0700

    late night expressions, feelings

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scene import SCENES, Option
+from copy import deepcopy
 
 history = []
 
@@ -9,7 +10,11 @@ def main():
         current_scene = current_scene.play()
         history.append(current_scene.name)
         if history[-6:] == ['library', 'east hall', 'west hall', 'east hall', 'library', 'west hall'] and 'continue reading' in history:
+            current_scene = deepcopy(current_scene)
             current_scene.options.options.append(Option('Go to the door', 'secret'))
+        if history.count('talk') >= 8 and current_scene.name == 'talk' and 'long' not in history:
+            current_scene = deepcopy(current_scene)
+            current_scene.options.options.append(Option('Hmm...', 'long'))
         if current_scene == SCENES['empty']:
             # should be unreachable because there is no end scene
             print("You won!")
